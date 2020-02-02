5cf838297771c43e867c691a6e5d72ee0c46c677
amansr1vastava <53302997+amansr1vastava@users.noreply.github.com>
2020-02-03 02:19:01 +0530

    Add files via upload

---
+++ crop.py
@@ -0,0 +1,26 @@
+import os
+import csv
+import re
+from PIL import Image
+
+if not os.path.exists('output'):
+    os.makedirs('output')
+
+with open('minor-export.csv','r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    for line in csv_reader:
+        filename = 'input/' + line['image']
+        im = Image.open(filename)
+        w, h = im.width, im.height
+        l = float(line['xmin'])
+        t = float(line['ymin'])
+        r = float(line['xmax'])
+        b = float(line['ymax'])
+        # Crop and save
+        im = im.crop((l,t,r,b))
+        filename = filename[5:]
+        im.save("output/" + filename)
+
+    
+
